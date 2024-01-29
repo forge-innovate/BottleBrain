b88ba4680aed69004a1637c51f803e0041101801
Lamsy <olamide.adetula.1744@gmail.com>
2024-01-29 19:25:15 +0100

    Input parameter

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Make the script itself executable
+#run chmod +x ./start.sh
+
+# Run the build command and then run the run command if build succeeds
+sunodo build && sunodo run
