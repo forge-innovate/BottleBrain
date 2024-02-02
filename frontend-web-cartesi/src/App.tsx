422985157cecb1274e4ef61e63ace72312d58975
forge-innovate <emmanuelmayowa95@gmail.com>
2024-02-02 09:56:48 +0100

    edited

--- frontend-web-cartesi/src/App.tsx
+++ frontend-web-cartesi/src/App.tsx
@@ -12,49 +12,67 @@ import { Notices } from "./Notices";
 import configFile from "./config.json";
 import { useWallets } from "@web3-onboard/react";
 
+<style>
+	@import
+	url('https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap');
+</style>;
 const config: any = configFile;
 
 const injected: any = injectedModule();
 init({
-  wallets: [injected],
-  chains: Object.entries(config).map(([k, v]: [string, any], i) => ({
-    id: k,
-    token: v.token,
-    label: v.label,
-    rpcUrl: v.rpcUrl,
-  })),
-  appMetadata: {
-    name: "Cartesi Rollups Test DApp",
-    icon: "<svg><svg/>",
-    description: "BottleBrain Rollups Test DApp",
-    recommendedInjectedWallets: [
-      { name: "MetaMask", url: "https://metamask.io" },
-    ],
-  },
+	wallets: [injected],
+	chains: Object.entries(config).map(([k, v]: [string, any], i) => ({
+		id: k,
+		token: v.token,
+		label: v.label,
+		rpcUrl: v.rpcUrl,
+	})),
+	appMetadata: {
+		name: "Cartesi Rollups Test DApp",
+		icon: "<svg><svg/>",
+		description: "BottleBrain Rollups Test DApp",
+		recommendedInjectedWallets: [
+			{ name: "MetaMask", url: "https://metamask.io" },
+		],
+	},
 });
 
 const App: FC = () => {
-  const [dappAddress, setDappAddress] = useState<string>(
-    "0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C"
-  );
-  const [inputValues, setInputValues] = useState<Record<string, string>>({});
+	const [dappAddress, setDappAddress] = useState<string>(
+		"0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C"
+	);
+	const [inputValues, setInputValues] = useState<Record<string, string>>({});
 
-  return (
-    <div className="">
-      <Navbar />
-      <Container>
-        <GraphQLProvider>
-          <Input dappAddress={dappAddress} />
-          {/* <h2>Reports</h2>
+	return (
+		<div className="">
+			<Navbar />
+			<div style={{ display: "flex", marginTop: "3%" }}>
+				<Container>
+					<GraphQLProvider>
+						<div style={{ display: "flex" }}>
+							<div>
+								<Input dappAddress={dappAddress} />
+							</div>
+							{/* <h2>Reports</h2>
           <Reports /> */}
-          {/* <h2>Notices</h2> */}
-          <Notices />
-          {/* <h2>Vouchers</h2>
+							{/* <h2>Notices</h2> */}
+							<div>
+								<h2
+									className="flex text-2xl font-bold mb-4 justify-center"
+									style={{ fontFamily: "Henny Penny", fontSize: "30px" }}
+								>
+									Result
+								</h2>
+								<Notices />
+							</div>
+						</div>
+						{/* <h2>Vouchers</h2>
           <Vouchers dappAddress={dappAddress} /> */}
-        </GraphQLProvider>
-      </Container>
-    </div>
-  );
+					</GraphQLProvider>
+				</Container>
+			</div>
+		</div>
+	);
 };
 
 export default App;
